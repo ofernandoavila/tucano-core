02ec9f6703f4128be793ead6854b1ead6be1b073
Fernando Avila <fernandoavilajunior@gmail.com>
2026-02-14 22:31:49 -0300

    fix(): Fix namespaces resolver to import shortcodes and controllers

--- src/Core/Factory.php
+++ src/Core/Factory.php
@@ -6,103 +6,109 @@ class Factory
 {
     private array $jsVars = [];
     private Config $config;
+    private Container $container;
 
-    public function __construct(
-        private Container $container
-    ) {
+    public function __construct(Container $container)
+    {
+        $this->container = new Container();
         $this->container->register(Router::class, fn() => new Router());
         $this->container->register(Database::class, fn() => new Database($this->container));
         $this->container->register(Migration::class, fn() => new Migration($this->container));
-
         $this->config = $this->container->resolve(Config::class);
-
         $this->container->register(Factory::class, fn() => $this);
     }
-
     public function install()
     {
-        $migration = $this->container->resolve(Migration::class);
-
-        $migration->createMigrationTable();
-        $migration->migrate();
+        $this->container
+             ->resolve(Migration::class)
+             ->createMigrationTable()
+             ->migrate();
     }
-
     public function uninstall()
     {
-        $migration = $this->container->resolve(Migration::class);
-
-        $migration->drop();
-        $migration->dropMigrationTable();
+        $this->container
+             ->resolve(Migration::class)
+             ->drop()
+             ->dropMigrationTable();
     }
-
     public function init()
     {
-        $this->addControllers();
-        $this->addShortcodes();
-        $this->addRouter();
+        $this->addControllers()
+             ->addShortcodes()
+             ->addRouter();
 
         if ($this->config->getWebComponentsConfig() && $this->config->getWebComponentsConfig()['global_js_variable']) {
-            add_action('wp_enqueue_scripts', function () {
-                wp_localize_script('jquery', $this->config->getWebComponentsConfig()['global_js_variable'], $this->jsVars);
+            \add_action('wp_enqueue_scripts', function () {
+                \wp_localize_script('jquery', $this->config->getWebComponentsConfig()['global_js_variable'], $this->jsVars);
             }, 1);
-
-            add_action('admin_enqueue_scripts', function () {
-                wp_localize_script('jquery', $this->config->getWebComponentsConfig()['global_js_variable'], $this->jsVars);
+            \add_action('admin_enqueue_scripts', function () {
+                \wp_localize_script('jquery', $this->config->getWebComponentsConfig()['global_js_variable'], $this->jsVars);
             }, 1);
         }
-
-
         return $this;
     }
-
     private function addRouter()
     {
-        if (file_exists($this->config->getRoutesPath())) {
+        if (\file_exists($this->config->getRoutesPath())) {
             $router = $this->container->resolve(Router::class);
             $container = $this->container;
             require $this->config->getRoutesPath();
             $router->Publish();
         }
-
         return $this;
     }
-
     private function addControllers()
     {
-        if (file_exists($this->config->getControllerDirPath())) {
-            $controllers = array_filter(scandir($this->config->getControllerDirPath()), fn($item) => $item != '.' && $item != '..');
-
+        if (\file_exists($this->config->getControllerDirPath())) {
+            $controllers = \array_filter(\scandir($this->config->getControllerDirPath()), fn($item) => $item != '.' && $item != '..');
             foreach ($controllers as $controller) {
-                $className = "\App\Http\Controller\\" . explode('.', $controller)[0];
+                $className = $this->getNamespace($this->config->getShortcodesDirPath() . '/' . $controller) . '\\' . explode('.', $controller)[0];
                 $class = new $className($this->container);
-
                 $this->container->register($class::class, fn() => $class);
             }
         }
-
         return $this;
     }
-
     private function addShortcodes()
     {
-        if (file_exists($this->config->getShortcodesDirPath())) {
-            $controllers = array_filter(scandir($this->config->getShortcodesDirPath()), fn($item) => $item != '.' && $item != '..');
-
+        if (\file_exists($this->config->getShortcodesDirPath())) {
+            $controllers = \array_filter(\scandir($this->config->getShortcodesDirPath()), fn($item) => $item != '.' && $item != '..');
             foreach ($controllers as $controller) {
-                $className = "\App\Shortcode\\" . explode('.', $controller)[0];
+                $className = $this->getNamespace($this->config->getShortcodesDirPath() . '/' . $controller) . '\\' . explode('.', $controller)[0];
                 $class = new $className($this->container);
-
                 $this->container->register($class::class, fn() => $class);
             }
         }
-
         return $this;
     }
-
     public function addJSVar(string $chave, mixed $valor)
     {
         $this->jsVars[$chave] = $valor;
-
         return $this;
+    }
+
+    private function getNamespace(string $filePath): ?string
+    {
+        if (!is_file($filePath) || !is_readable($filePath))
+            return null;
+
+        $code = file_get_contents($filePath);
+        
+        if ($code === false)
+            return null;
+
+        $tokens = token_get_all($code);
+        $namespace = '';
+
+        foreach ($tokens as $key => $token) {
+            if (is_array($token)) {
+                if ($token[0] === T_NAMESPACE) {
+                    $namespace = $tokens[$key + 2][1];
+                    break;
+                }
+            }
+        }
+
+        return $namespace;
     }
 }
